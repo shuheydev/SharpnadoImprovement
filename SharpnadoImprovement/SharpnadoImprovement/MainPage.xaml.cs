cdc5c807daa8964e1bff6f9b52eeeed406d86398
Shuhey <shuhey.dev@gmail.com>
2020-04-26 15:30:28 +0900

    Initial commit

---
+++ SharpnadoImprovement/SharpnadoImprovement/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SharpnadoImprovement
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<Person> _people;
+        public ObservableCollection<Person> People { 
+            get=> _people;
+            set
+            {
+                _people = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.People = SampleDataGenerator.GetPeople();
+
+            this.BindingContext = this;
+        }
+    }
+}
---
+++ SharpnadoImprovement/SharpnadoImprovement/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SharpnadoImprovement
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public ImageSource ImageSrc { get; set; }
+    }
+}
